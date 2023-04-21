365e3f886453f49dff08af843b5ec6a23a14d5fa
maheshsp <maheshsp1809@gmail.com>
2023-04-22 01:03:54 +0530

    case_permute

---
+++ JavaRelated/recursion/Case_permute.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class Case_permute {
+    public static void spacePermute(String str) {
+        spacePermuteHelper("", str);
+    }
+
+    private static void spacePermuteHelper(String prefix, String suffix) {
+        if (suffix.length() == 0) {
+            System.out.print("(" + prefix + ")");
+            return;
+        }
+        spacePermuteHelper(prefix + suffix.charAt(0), suffix.substring(1));
+        spacePermuteHelper(prefix + Character.toUpperCase(suffix.charAt(0)), suffix.substring(1));
+
+    }
+
+    public static void main(String[] args) {
+        String str = "ab";
+        spacePermute(str);
+    }
+}
